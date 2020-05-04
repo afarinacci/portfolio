c68205d1f894a9e0b01fc811eba4aeb28eecd191
AFarinacci <andreafarinacci@me.com>
2020-05-04 15:55:48 -0500

    Updates

---
+++ precache-manifest.6b8f2744c01f9dbfe74020a8b1ed5677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc64e662c59c662d19734b203664b74",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f313e1ddda1f4230cc21",
+    "url": "/portfolio/static/css/main.905d83f3.chunk.css"
+  },
+  {
+    "revision": "e379dc7efef4154f77fc",
+    "url": "/portfolio/static/js/2.12a4b06d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.12a4b06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f313e1ddda1f4230cc21",
+    "url": "/portfolio/static/js/main.3ef1d82a.chunk.js"
+  },
+  {
+    "revision": "794a85b0707ea95ccdb5",
+    "url": "/portfolio/static/js/runtime-main.a3c03618.js"
+  }
+]);
